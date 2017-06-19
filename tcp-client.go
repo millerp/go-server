152a5c5c7e4c2fe7fa2adc17d5c06875ab03baa7
Miller P. Magalhães <miller@mpdev.com.br>
2017-06-19 17:36:33 -0300

    first commit

---
+++ tcp-client.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"fmt"
+	"net"
+)
+
+func main() {
+
+	// connect to this socket
+	conn, _ := net.Dial("tcp", "127.0.0.1:1200")
+	for {
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text + "\n")
+		// listen for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
